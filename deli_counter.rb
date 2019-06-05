dc2736bc0bd9065e711d4a84bc1ccec00cd7f965
Kelliann Lafferty <kell10192@gmail.com>
2019-06-05 20:34:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,4 +21,7 @@ def now_serving(katz_deli)
 if katz_deli.size == 0 
   puts "There is nobody waiting to be served!"
 else 
+  puts "Currently serving #{katz_deli.first}."
+  katz_deli.shift 
+end
 end
